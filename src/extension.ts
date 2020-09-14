3c2fec9bee0aed7977541b6b9e3eba0c6e15e556
Teja Sophista <tejanium@yahoo.com>
2020-09-14 14:30:45 +0100

    view

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import { Permalink } from './model/permalink';
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('copy-github-permalink.copy', async () => {
+	let copy = vscode.commands.registerCommand('copy-github-permalink.copy', async () => {
 		const editor = vscode.window.activeTextEditor;
 
 		if (editor && editor.document.uri.scheme === 'file') {
@@ -20,7 +20,31 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(copy);
+
+	let view = vscode.commands.registerCommand('copy-github-permalink.view', async () => {
+		const input = await vscode.window.showInputBox()
+		const [blob, position] = input?.split("blob").pop()?.split("/").slice(2).join("/").split("#") || [];
+
+		if (blob) {
+			const uri = await vscode.workspace.findFiles(`**/${blob}`);
+			const textDocument = await vscode.workspace.openTextDocument(uri[0]);
+			const opts = { preview: false }
+
+			if (position) {
+				const [start, end] = position.replace(/L/g, "").split("-");
+				const startPosition = new vscode.Position(parseInt(start) - 1, 0);
+				const endPosition = new vscode.Position(parseInt(end), 0);
+				const range = new vscode.Range(startPosition, endPosition);
+
+				Object.assign(opts, { selection: range })
+			}
+
+			vscode.window.showTextDocument(textDocument, opts)
+		}
+	});
+
+	context.subscriptions.push(view);
 }
 
 function getBranch(): string {
