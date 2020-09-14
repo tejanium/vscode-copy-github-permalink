16feb2d21b456652c86bca2dc6fb90ea4c31fe5a
Teja Sophista <tejanium@yahoo.com>
2020-09-14 14:58:55 +0100

    lint

--- src/extension.ts
+++ src/extension.ts
@@ -23,13 +23,13 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(copy);
 
 	let view = vscode.commands.registerCommand('copy-github-permalink.view', async () => {
-		const input = await vscode.window.showInputBox()
+		const input = await vscode.window.showInputBox();
 		const [blob, position] = input?.split("blob").pop()?.split("/").slice(2).join("/").split("#") || [];
 
 		if (blob) {
 			const uri = await vscode.workspace.findFiles(`**/${blob}`);
 			const textDocument = await vscode.workspace.openTextDocument(uri[0]);
-			const opts = { preview: false }
+			const opts = { preview: false };
 
 			if (position) {
 				const [start, end] = position.replace(/L/g, "").split("-");
@@ -37,10 +37,10 @@ export function activate(context: vscode.ExtensionContext) {
 				const endPosition = new vscode.Position(parseInt(end), 0);
 				const range = new vscode.Range(startPosition, endPosition);
 
-				Object.assign(opts, { selection: range })
+				Object.assign(opts, { selection: range });
 			}
 
-			vscode.window.showTextDocument(textDocument, opts)
+			vscode.window.showTextDocument(textDocument, opts);
 		}
 	});
 
